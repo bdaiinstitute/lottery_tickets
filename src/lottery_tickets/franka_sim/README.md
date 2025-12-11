7120c8d6925423c60d1dc7415f03a95a15ef7479
Ondrej Biza <obiza@theaiinstitute.com>
2025-12-11 15:31:46 -0500

    Add our frankasim.

---
+++ src/lottery_tickets/franka_sim/README.md
@@ -0,0 +1,8 @@
+# Intro
+This package provide a simple Franka arm and Robotiq Gripper simulator written in Mujoco.
+It includes a state-based and a vision-based Franka lift cube task environment.
+
+# Credits
+- This simulation is initially built by [Kevin Zakka](https://kzakka.com/).
+- This simulation was open-sourced in [SERL](https://github.com/rail-berkeley/serl) under Apache-2.0.
+- We added sparse rewards and realistic control.
---
+++ src/lottery_tickets/franka_sim/franka_sim/envs/panda_reach_gym_env.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from franka_sim.envs import PandaPickCubeGymEnv
+
+REWARD_THRESHOLD = 0.8
+
+
+class PandaReachCubeGymEnv(PandaPickCubeGymEnv):
+    def _compute_reward(self) -> float:
+        block_pos = self._data.sensor("block_pos").data
+        tcp_pos = self._data.sensor("2f85/pinch_pos").data
+        dist = np.linalg.norm(block_pos - tcp_pos)
+        r_close = np.exp(-20 * dist)
+        return r_close
