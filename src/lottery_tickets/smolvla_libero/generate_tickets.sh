b67b2612d8d3bd0dafc3eb71a5f4c495f493ece0
Wil Thomason <wthomason@theaiinstitute.com>
2026-01-20 12:09:23 -0500

    Improved smolvla generation wrapper

--- src/lottery_tickets/smolvla_libero/generate_tickets.sh
+++ src/lottery_tickets/smolvla_libero/generate_tickets.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
-set -e
+NUM_TICKETS=1
+TASK=libero_spatial
+EPSILON=None
+BATCH_SIZE=1
 
-NUM_TICKETS=$1
-EPSILON=$2
-TASK=$3
-OUTPUT_PATH=$4
-shift 3
+for arg in "$@"; do
+    case $arg in
+        --num_tickets=*)
+            NUM_TICKETS="${arg#*=}"
+            ;;
+        --task=*)
+            TASK="${arg#*=}"
+            ;;
+        --output_dir=*)
+            OUTPUT_PATH="${arg#*=}"
+            ;;
+        --num_episodes=*)
+            NUM_EPISODES="${arg#*=}"
+            ;;
+        --batch_size=*)
+            BATCH_SIZE="${arg#*=}"
+            ;;
+        --epsilon=*)
+            EPSILON="${arg#*=}"
+            ;;
+        *)
+            echo "Unknown argument: $arg"
+            exit 1
+            ;;
+    esac
+done
 
+# Required args check
+: "${OUTPUT_PATH:?Missing --output_dir}"
+: "${NUM_EPISODES:?Missing --num_episodes}"
 
 echo "Generating $NUM_TICKETS tickets with epsilon=$EPSILON to $OUTPUT_PATH"
 
 for i in $(seq 1 "$NUM_TICKETS"); do
     seed=$((1000+i))
     echo "Generating ticket $i of $NUM_TICKETS..."
-    uv run python evaluate.py --policy.path="HuggingFaceVLA/smolvla_libero"  --env.type=libero --env.task="$TASK"  --eval.batch_size=5  --eval.n_episodes=25 --output_dir="$OUTPUT_PATH" --eval_mode=NEW_TICKET  --seed=$seed --epsilon="$EPSILON"
+    uv run python evaluate.py --policy.path="HuggingFaceVLA/smolvla_libero"  --env.type=libero --env.task="$TASK"  --eval.batch_size="$BATCH_SIZE"  --eval.n_episodes="$NUM_EPISODES" --output_dir="$OUTPUT_PATH" --eval_mode=NEW_TICKET  --seed=$seed --epsilon="$EPSILON"
 done
 
 echo "Successfully generated $NUM_TICKETS tickets!"
