2912932c1e32370d582d8938dabbe8e6ad160b0e
Wil Thomason <wthomason@theaiinstitute.com>
2026-01-28 15:57:03 -0500

    Fixes to epsilon search plotting script; untested

--- src/lottery_tickets/robomimic_dppo_lt/plot_epsilon_search_results.py
+++ src/lottery_tickets/robomimic_dppo_lt/plot_epsilon_search_results.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from math import ceil, floor
+from math import floor
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -22,53 +22,28 @@ def load_ticket_results(task_name: str, results_path: Path) -> TicketResult | No
         return None
 
     ticket_name = results_path.name
-    epsilons = []
-    rewards = []
-    successes = []
-    for epsilon_dir in sorted(results_path.iterdir()):
-        epsilon_results_prefix = list(
-            (epsilon_dir / "outputs" / task_name).glob("envs*/eval_*/noise_idx_*/")
+    epsilons = [float(results_path.parent.parent.name)]
+    rewards_path = results_path / "reward_matrix.npy"
+    successes_path = results_path / "success_matrix.npy"
+    if not rewards_path.exists() or not successes_path.exists():
+        print(
+            f"Warning! {results_path} doesn't contain the expected results files; skipping!"
         )
-        if len(epsilon_results_prefix) < 1:
-            print(
-                f"Warning! {epsilon_dir} doesn't contain the expected results files; skipping!"
-            )
-            continue
-
-        assert len(epsilon_results_prefix) == 1
-        rewards_path = epsilon_results_prefix[0] / "reward_matrix.npy"
-        successes_path = epsilon_results_prefix[0] / "success_matrix.npy"
-        if not rewards_path.exists() or not successes_path.exists():
-            print(
-                f"Warning! {epsilon_dir} doesn't contain the expected results files; skipping!"
-            )
-            continue
-
-        rewards.append(np.load(rewards_path))
-        epsilons.append(float(epsilon_dir.name))
-        successes.append(np.load(successes_path))
-
-    if not epsilons:
-        raise RuntimeError(
-            f"{results_path} was given as a results path but contained no results files!"
-        )
+        return None
 
     return TicketResult(
         ticket=ticket_name,
         epsilons=epsilons,
-        rewards=np.stack(rewards),
-        successes=np.stack(successes),
+        rewards=np.load(rewards_path)[np.newaxis, ...],
+        successes=np.load(successes_path)[np.newaxis, ...],
     )
 
 
 def take_top_tickets(
-    ticket_results: list[TicketResult | None], top_n: int, split_percentage: float
+    ticket_results: list[TicketResult], top_n: int, split_percentage: float
 ) -> list[TicketResult]:
     split_tickets = []
     for ticket_result in ticket_results:
-        if ticket_result is None:
-            continue
-
         split_idx = floor(split_percentage * ticket_result.successes.shape[1])
         split_tickets.append(
             (
@@ -122,6 +97,7 @@ def main(
     output_dir: Path,
     top_n: int = 10,
     eval_split: float = 0.5,
+    n_envs: int = 100,
 ) -> None:
     success_plot, success_axes = plt.subplots(tight_layout=True)
     success_axes.title.set_text(f"{task_name}: Success Rate vs. Epsilon")
@@ -134,10 +110,38 @@ def main(
     reward_axes.set_xlabel("Epsilon")
     reward_axes.set_ylabel("Average Reward")
 
-    ticket_results = [
-        load_ticket_results(task_name, ticket_dir)
-        for ticket_dir in (results_root / task_name).iterdir()
-    ]
+    aggregated_ticket_results: dict[str, TicketResult] = {}
+    for ticket_dir in results_root.glob(f"0.*/{task_name}/envs{n_envs}*"):
+        epsilon_ticket_result = load_ticket_results(task_name, ticket_dir)
+        if epsilon_ticket_result is None:
+            continue
+
+        if epsilon_ticket_result.ticket not in aggregated_ticket_results:
+            aggregated_ticket_results[epsilon_ticket_result.ticket] = (
+                epsilon_ticket_result
+            )
+            continue
+
+        # Otherwise, extend the ticket with the new epsilon
+        aggregated_ticket_result = aggregated_ticket_results[
+            epsilon_ticket_result.ticket
+        ]
+        assert len(epsilon_ticket_result.epsilons) == 1
+        assert (
+            epsilon_ticket_result.epsilons[0] not in aggregated_ticket_result.epsilons
+        )
+
+        aggregated_ticket_result.epsilons.extend(epsilon_ticket_result.epsilons)
+        aggregated_ticket_result.rewards = np.concatenate(
+            [aggregated_ticket_result.rewards, epsilon_ticket_result.rewards], axis=0
+        )
+        aggregated_ticket_result.successes = np.concatenate(
+            [aggregated_ticket_result.successes, epsilon_ticket_result.successes],
+            axis=0,
+        )
+
+    ticket_results = list(aggregated_ticket_results.values())
+
     for ticket_result in take_top_tickets(ticket_results, top_n, eval_split):
         plot_ticket_success(success_axes, ticket_result)
         plot_ticket_rewards(reward_axes, ticket_result)
